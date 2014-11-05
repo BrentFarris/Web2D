e57e6d7f07e06fafee6b1904cc6d5b40ef6a76ff
Brent Farris <brent@farrisarts.com>
2014-11-04 16:31:24 -0800

    Added default button effects

--- button.js
+++ button.js
@@ -19,11 +19,29 @@ var $Button = function(rect, label, callback) {
 		this.label.rect.y = this.rect.y + ((this.rect.height - this.label.rect.height) * 0.5);
 	}
 	
+	if (label != null) {
+		this.SetText(label.text);
+	}
+	
 	this.Draw = function(canvas) {
 		this.label.Draw(canvas);
 		canvas.context.lineWidth = this.strokeWidth;
 		canvas.context.strokeRect(this.rect.x, this.rect.y, this.rect.width, this.rect.height);
 	};
+
+	//////////////////////////////
+	//	Default button behavior	//
+	//////////////////////////////
+	this.HoverOverEffect = function() {
+		this.strokeWidth = 2;
+	};
+
+	this.HoverOutEffect = function() {
+		this.strokeWidth = 1;
+	};
+	
+	b.onMouseOver.Register(this.HoverOverEffect, this);
+	b.onMouseOut.Register(this.HoverOutEffect, this);
 };
 
 $Button.prototype = new $UI();
