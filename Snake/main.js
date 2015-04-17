d1239670ac82fc95e6a7ee3cbafb0b3ffe92696d
Brent Farris <brent@farrisarts.com>
2015-04-16 18:53:04 -0700

    Rectangle does not need to be logging offset

--- Snake/main.js
+++ Snake/main.js
@@ -1,11 +1,27 @@
 registerCanvas(document.getElementById("canvas"), 1.0, 1.0);
 
+var speed = 1.0;
+var lastMove = Time.time;
 var player = new $Rectangle(50, 50, 25, 25, new $Color(0, 0, 0));
+var food = new $Rectangle(300, 50, 25, 25, new $Color(255, 0, 0));
 
-Canvas.drawing.Register(player.Draw, player);
-
-function CheckCollision() {
-	
+function Move() {
+	if (lastMove < Time.time - speed) {
+		lastMove = Time.time;
+		
+		player.x += player.width;
+	}
 }
 
+function CheckCollision() {
+	if (player.Intersects(food)) {
+		food.x += 100;
+		food.y += 100;
+	}
+}
+
+Canvas.drawing.Register(player.Draw, player);
+Canvas.drawing.Register(food.Draw, food);
+
 Canvas.updating.Register(CheckCollision);
+Canvas.updating.Register(Move);
